da1a0eb407a740787e53a517b708542d2adc3a78
shuaiyin <shuaiyin.top@foxmail.com>
2021-04-06 15:41:37 +0800

    fix(两数之和): 更新快慢指针法(#1)

--- practise/leetcode/editor/cn/[1]两数之和.js
+++ practise/leetcode/editor/cn/[1]两数之和.js
@@ -1,17 +1,17 @@
-//给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。 
+//给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
 //
-// 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。 
+// 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
 //
-// 
 //
-// 示例: 
+//
+// 示例:
 //
 // 给定 nums = [2, 7, 11, 15], target = 9
 //
 //因为 nums[0] + nums[1] = 2 + 7 = 9
 //所以返回 [0, 1]
-// 
-// Related Topics 数组 哈希表 
+//
+// Related Topics 数组 哈希表
 // 👍 9381 👎 0
 
 
@@ -54,4 +54,13 @@ console.log(twoSum([2, 7, 11, 15], 9));
 //         if (index !== undefined) return [index, i];
 //         map[currentValue] = i;
 //     }
+// };
+
+// 快慢指针
+// var twoSum = function(nums, target) {
+//   for (let i = 0; i < nums.length - 1; i++) {
+//     for (let j = i + 1; j < nums.length; j++) {
+//       if (nums[i] + nums[j] === target) return [i, j];
+//     }
+//   }
 // };
