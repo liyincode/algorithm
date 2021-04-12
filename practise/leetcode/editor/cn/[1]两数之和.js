9a4b851a519ffc635217751a2527e819be33f727
Shuaiyin <shuaiyin.top@foxmail.com>
2021-04-13 07:08:40 +0800

    fix: [1]两数之和，优化 for 循环，少一次执行

--- practise/leetcode/editor/cn/[1]两数之和.js
+++ practise/leetcode/editor/cn/[1]两数之和.js
@@ -32,7 +32,7 @@ console.log(twoSum([2, 7, 11, 15], 9));
 
 // 1 暴力法
 // var twoSum = function(nums, target) {
-//     for (let i = 0; i < nums.length; i++) {
+//     for (let i = 0; i < nums.length - 1; i++) {
 //         for (let j =  i + 1; j < nums.length; j++) {
 //             const sum = nums[i] + nums[j];
 //             if (sum === target) return [i, j];
