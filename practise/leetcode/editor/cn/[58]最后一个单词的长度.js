1adac1a4ffab451b941a0afbd4056743f8fa7d03
shuaiyin <shuaiyin.top@foxmail.com>
2021-12-02 17:52:03 +0800

    fix: [58]最后一个单词的长度，更新单指针解法

--- practise/leetcode/editor/cn/[58]最后一个单词的长度.js
+++ practise/leetcode/editor/cn/[58]最后一个单词的长度.js
@@ -1,32 +1,32 @@
-//给你一个字符串 s，由若干单词组成，单词之间用空格隔开。返回字符串中最后一个单词的长度。如果不存在最后一个单词，请返回 0 。 
+//给你一个字符串 s，由若干单词组成，单词之间用空格隔开。返回字符串中最后一个单词的长度。如果不存在最后一个单词，请返回 0 。
 //
-// 单词 是指仅由字母组成、不包含任何空格字符的最大子字符串。 
+// 单词 是指仅由字母组成、不包含任何空格字符的最大子字符串。
 //
-// 
 //
-// 示例 1： 
 //
-// 
+// 示例 1：
+//
+//
 //输入：s = "Hello World"
 //输出：5
-// 
 //
-// 示例 2： 
 //
-// 
+// 示例 2：
+//
+//
 //输入：s = " "
 //输出：0
-// 
 //
-// 
 //
-// 提示： 
 //
-// 
-// 1 <= s.length <= 104 
-// s 仅有英文字母和空格 ' ' 组成 
-// 
-// Related Topics 字符串 
+//
+// 提示：
+//
+//
+// 1 <= s.length <= 104
+// s 仅有英文字母和空格 ' ' 组成
+//
+// Related Topics 字符串
 // 👍 303 👎 0
 
 
@@ -46,3 +46,26 @@ var lengthOfLastWord = function(s) {
     return end - start;
 };
 //leetcode submit region end(Prohibit modification and deletion)
+
+console.log(lengthOfLastWord('hello world'));
+
+
+// 单指针法
+// var lengthOfLastWord = function(s) {
+//     if (s === ' ') return 0;
+//     let lastWord = '';
+//     s = s.trim();
+//
+//     for (let i = s.length - 1; i >= 0; i--) {
+//         // 多个单词，当指针发现有空格时
+//         if (s[i] === ' ') {
+//             lastWord = s.substring(i + 1, s.length);
+//             return lastWord.length;
+//         }
+//
+//         // 只有一个单词时
+//         if (i === 0) {
+//             return s.length;
+//         }
+//     }
+// };
