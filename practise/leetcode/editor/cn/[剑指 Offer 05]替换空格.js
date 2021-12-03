a767d5c8463f56aa78ba4957fc42be76f991c7a7
shuaiyin <shuaiyin.top@foxmail.com>
2021-12-03 14:57:48 +0800

    fix: [剑指 Offer 05]替换空格，修改解法

--- practise/leetcode/editor/cn/[剑指 Offer 05]替换空格.js
+++ practise/leetcode/editor/cn/[剑指 Offer 05]替换空格.js
@@ -1,17 +1,17 @@
-//请实现一个函数，把字符串 s 中的每个空格替换成"%20"。 
+//请实现一个函数，把字符串 s 中的每个空格替换成"%20"。
 //
-// 
 //
-// 示例 1： 
+//
+// 示例 1：
 //
 // 输入：s = "We are happy."
-//输出："We%20are%20happy." 
+//输出："We%20are%20happy."
 //
-// 
 //
-// 限制： 
 //
-// 0 <= s 的长度 <= 10000 
+// 限制：
+//
+// 0 <= s 的长度 <= 10000
 // 👍 96 👎 0
 
 
@@ -21,18 +21,22 @@
  * @return {string}
  */
 var replaceSpace = function(s) {
-    let array = new Array(s.length * 3);
-    for (let i = 0; i < array.length; i++) {
-        const char = s.charAt(i);
-        if (char === ' ') {
-            array[i] = '%20';
-        } else {
-            array[i] = char;
-        }
-    }
 
-    return array.join().replace(/,/g, '');
 };
 //leetcode submit region end(Prohibit modification and deletion)
 
 console.log(replaceSpace('We are happy.'))
+
+
+// var replaceSpace = function(s) {
+//     let result = '';
+//     for (let i = 0; i < s.length; i++) {
+//         if (s.charAt(i) === ' ') {
+//             result += '%20';
+//         } else {
+//             result += s.charAt(i);
+//         }
+//     }
+//
+//     return result;
+// };
