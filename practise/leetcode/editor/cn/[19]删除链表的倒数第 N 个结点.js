70719ec98843467d40a67ee59670b6baa0a36ad1
shuaiyin <shuaiyin.top@foxmail.com>
2021-12-15 16:44:31 +0800

    fix: [19]删除链表的倒数第 N 个结点，添加解法注释

--- practise/leetcode/editor/cn/[19]删除链表的倒数第 N 个结点.js
+++ practise/leetcode/editor/cn/[19]删除链表的倒数第 N 个结点.js
@@ -1,41 +1,41 @@
-//给你一个链表，删除链表的倒数第 n 个结点，并且返回链表的头结点。 
+//给你一个链表，删除链表的倒数第 n 个结点，并且返回链表的头结点。
 //
-// 进阶：你能尝试使用一趟扫描实现吗？ 
+// 进阶：你能尝试使用一趟扫描实现吗？
 //
-// 
 //
-// 示例 1： 
 //
-// 
+// 示例 1：
+//
+//
 //输入：head = [1,2,3,4,5], n = 2
 //输出：[1,2,3,5]
-// 
 //
-// 示例 2： 
 //
-// 
+// 示例 2：
+//
+//
 //输入：head = [1], n = 1
 //输出：[]
-// 
 //
-// 示例 3： 
 //
-// 
+// 示例 3：
+//
+//
 //输入：head = [1,2], n = 1
 //输出：[1]
-// 
 //
-// 
 //
-// 提示： 
 //
-// 
-// 链表中结点的数目为 sz 
-// 1 <= sz <= 30 
-// 0 <= Node.val <= 100 
-// 1 <= n <= sz 
-// 
-// Related Topics 链表 双指针 
+//
+// 提示：
+//
+//
+// 链表中结点的数目为 sz
+// 1 <= sz <= 30
+// 0 <= Node.val <= 100
+// 1 <= n <= sz
+//
+// Related Topics 链表 双指针
 // 👍 1319 👎 0
 
 
@@ -57,63 +57,90 @@ var removeNthFromEnd = function(head, n) {
 };
 
 
-
 //leetcode submit region end(Prohibit modification and deletion)
 function ListNode(val) {
-    this.val = val;
-    this.next = null;
+  this.val = val;
+  this.next = null;
 }
 
 function display(head) {
-    let curr = head;
-    while (curr !== null) {
-        console.log(curr)
-        curr = curr.next;
-    }
+  let curr = head;
+  while (curr !== null) {
+    console.log(curr.val)
+    curr = curr.next;
+  }
 }
 
 let node1 = new ListNode(1);
 let node2 = new ListNode(2);
-// let node3 = new ListNode(3);
-// let node4 = new ListNode(4);
-// let node5 = new ListNode(5);
+let node3 = new ListNode(3);
+let node4 = new ListNode(4);
+let node5 = new ListNode(5);
 node1.next = node2;
-// node2.next = node3;
-// node3.next = node4;
-// node4.next = node5;
+node2.next = node3;
+node3.next = node4;
+node4.next = node5;
 
 
-const result = removeNthFromEnd(node1, 1);
-console.log('result = ')
+const result = removeNthFromEnd(node1, 5);
 display(result);
 
-
-// 获取长度计算位置
 //var removeNthFromEnd = function(head, n) {
-//     let length = getLength(head);
-//
-//     // 设置头节点，方便删除第一个节点
-//     let dummy = new ListNode(0);
-//     dummy.next = head;
-//     let curr = dummy;
-//     for (let i = 0; i < length - n; i++) {
+//     // 计算长度
+//     let count = 0;
+//     let curr = head;
+//     while (curr) {
+//         count++
 //         curr = curr.next;
 //     }
-//     // 删除倒数第 n 个节点
-//     curr.next = curr.next.next;
 //
-//     return dummy.next;
+//     // 找到目标节点前一个节点的位置
+//     let targetIndex = count - n;
+//     // 如果 12345 删除倒数第 5 个，或者 1 删除倒数第 1 个这种，特殊处理
+//     if (targetIndex === 0) {
+//         let newHead = new ListNode();
+//         newHead.next = head;
+//         newHead.next = newHead.next.next;
+//         return newHead.next;
+//     }
+//     count = 0;
+//     curr = head;
+//     while (curr) {
+//         count++;
+//         if (count === targetIndex) {
+//             // 删除目标节点
+//             curr.next = curr.next.next;
+//             return head;
+//         }
+//         curr = curr.next;
+//     }
+//
+//     return head;
 // };
-//
-// function getLength(head) {
-//     let curr = head;
+
+// 获取长度计算位置
+// var removeNthFromEnd = function(head, n) {
+//     // 计算长度
 //     let length = 0;
+//     let curr = head;
 //     while (curr) {
 //         length++;
 //         curr = curr.next;
 //     }
-//     return length;
-// }
+//
+//     // 设置头节点，方便删除第一个节点的情况
+//     let newHead = new ListNode();
+//     newHead.next = head;
+//     curr = newHead;
+//     for (let i = 0; i < length - n; i++) {
+//         // curr 指向哪里和 length - n 的关系需要捋清楚，比较重要
+//         curr = curr.next;
+//     }
+//     // 删除目标节点
+//     curr.next = curr.next.next;
+//
+//     return newHead.next;
+// };
 
 // 快慢双指针
 //var removeNthFromEnd = function(head, n) {
