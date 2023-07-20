9016b76b540da035433c5d81819500f627dc2dc3
liyin <shuaiyin.top@foxmail.com>
2023-07-20 16:36:15 +0800

    fix: [剑指 Offer 32 - I]从上到下打印二叉树.js，更新队列方式

--- leetcode/editor/cn/[剑指 Offer 32 - I]从上到下打印二叉树.js
+++ leetcode/editor/cn/[剑指 Offer 32 - I]从上到下打印二叉树.js
@@ -39,49 +39,75 @@
  * @param {TreeNode} root
  * @return {number[]}
  */
-var levelOrder = function(root) {
-    const result = []
-    const map = {}
 
-    if (root) {
-        map[0] = [root.val]
-    } else {
-        return result
-    }
-
-    start(root)
-
-    function start(node, layer = 1) {
-        console.log(node.val, layer)
-        if ((node.left || node.right) && !map[layer]){
-            map[layer] = []
-        }
-        if (node.left) {
-            map[layer].push(node.left.val)
-        }
-        if (node.right) {
-            map[layer].push(node.right.val)
-        }
-
-        if (node.left || node.right) {
-            layer++
-        }
-
-        if (node.left) {
-            start(node.left, layer)
-        }
-        if (node.right) {
-            start(node.right, layer)
-        }
-        console.log(map)
-        layer--
-    }
-
-    console.log(map)
-    Object.values(map).forEach(item => {
-        result.push(item)
-    })
-
-    return result.flat()
-};
 //leetcode submit region end(Prohibit modification and deletion)
+/*
+    使用队列方式，先将 root 放入队列，从队列出根元素放入结果数组，然后将根元素的左右树放入队列
+    ，继续将左子树从队列取出放入结果数组，然后再将左子树的左右子树放入队列，依次循环往复
+ */
+//var levelOrder = function (root) {
+//     if (!root) {
+//         return []
+//     }
+//
+//     const result = []
+//     const queue = []
+//
+//     queue.push(root)
+//     while (queue.length) {
+//         const target = queue.shift()
+//         target.left && queue.push(target.left)
+//         target.right && queue.push(target.right)
+//
+//         result.push(target.val)
+//     }
+//
+//     return result
+// }
+
+// var levelOrder = function(root) {
+//     const result = []
+//     const map = {}
+//
+//     if (root) {
+//         map[0] = [root.val]
+//     } else {
+//         return result
+//     }
+//
+//     start(root)
+//
+//     function start(node, layer = 1) {
+//         console.log(node.val, layer)
+//         if ((node.left || node.right) && !map[layer]){
+//             map[layer] = []
+//         }
+//         if (node.left) {
+//             map[layer].push(node.left.val)
+//         }
+//         if (node.right) {
+//             map[layer].push(node.right.val)
+//         }
+//
+//         if (node.left || node.right) {
+//             layer++
+//         }
+//
+//         if (node.left) {
+//             start(node.left, layer)
+//         }
+//         if (node.right) {
+//             start(node.right, layer)
+//         }
+//         console.log(map)
+//         layer--
+//     }
+//
+//     console.log(map)
+//     Object.values(map).forEach(item => {
+//         result.push(item)
+//     })
+//
+//     return result.flat()
+// };
+
