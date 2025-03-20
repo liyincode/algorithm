aa6a78a4e0ddb388e38c270a7cc22eb7834ee46b
liyin <liyincode@gmail.com>
2025-03-20 11:56:26 +0800

    feat: 树的深度优先遍历和广度优先遍历.ts, 添加非递归遍历

--- leetcode/editor/cn/树的深度优先遍历和广度优先遍历.ts
+++ leetcode/editor/cn/树的深度优先遍历和广度优先遍历.ts
@@ -18,7 +18,25 @@ class treeNode {
         if (this.right) {
             this.right.preOrder();
         }
+    }
 
+    // 非递归前序遍历
+    preOrderIterative() {
+        const stack : treeNode[] = [this]
+
+        while (stack.length) {
+            const node = stack.pop()
+            console.log(node.data)
+
+            // 先压右子节点，再压左子节点，到时候先打印左子节点，后右子节点
+            if (node.right) {
+                stack.push(node.right)
+            }
+
+            if (node.left) {
+                stack.push(node.left)
+            }
+        }
     }
 
     // 中序遍历
@@ -33,6 +51,27 @@ class treeNode {
 
     }
 
+    // 非递归中序遍历
+    inOrderIterative() {
+        const stack: treeNode[] = []
+        let curr: treeNode = this
+
+        while (curr || stack.length) {
+            // 遍历找到左叶子节点
+            while (curr) {
+                stack.push(curr)
+                curr = curr.left
+            }
+
+            // 出栈打印
+            curr = stack.pop()
+            console.log(curr.data)
+
+            // 转向右子节点
+            curr = curr.right
+        }
+    }
+
     // 后序遍历
     postOrder() {
         if (this.left) {
@@ -42,6 +81,28 @@ class treeNode {
             this.right.postOrder()
         }
         console.log(this.data)
+    }
+
+    postOrderIterative() {
+        const stack1: treeNode[] = [this]
+        const stack2: treeNode[] = []
+
+        while (stack1.length) {
+            const node = stack1.pop()
+            stack2.push(node)
+
+            if (node.left) {
+                stack1.push(node.left)
+            }
+            if (node.right) {
+                stack1.push(node.right)
+            }
+        }
+
+        while (stack2.length) {
+            const node = stack2.pop()
+            console.log(node.data)
+        }
     }
 
     // 广度优先遍历
@@ -73,12 +134,18 @@ let root = new treeNode(1, node2, node3)
 // 调用各种遍历方法
 console.log("Pre-order traversal:");
 root.preOrder();
+console.log("Pre-order-iterative traversal :")
+root.preOrderIterative()
 
 console.log("In-order traversal:");
 root.inOrder();
+console.log("In-order-iterative traversal :")
+root.inOrderIterative();
 
 console.log("Post-order traversal:");
 root.postOrder();
+console.log("Post-order-iterative traversal:");
+root.postOrderIterative();
 
 console.log("BFS traversal:");
 root.bfs();
